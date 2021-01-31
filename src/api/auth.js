f2db99bda3ab0569268ba469811e75bec247ce2c
Victor Garcia <v.garcia.mestre@gmail.com>
2021-01-31 17:16:14 +0100

    Auth of users connected to endpoint and logout

--- src/api/auth.js
+++ src/api/auth.js
@@ -11,12 +11,51 @@ export function getAccessToken (){
     return willExpireToken(accessToken) ? null : accessToken;
 }
 
-export function getRefreshToken(){
+export function getRefreshTokenApi(){
     const refreshToken = localStorage.getItem(REFRESH_TOKEN);
     if (!refreshToken || refreshToken === "null"){
         return null;
     }
     return willExpireToken(refreshToken) ? null : refreshToken;
+}
+
+export function refreshAccessToken(refreshToken){
+    const url = `${basePath}/${apiVersion}/-refresh-access-token`;
+    const bodyObject={
+        refreshToken:refreshToken
+    }
+    const params={
+        method:"POST",
+        body: JSON.stringify(bodyObject),
+        headers:{
+            "Content-Type":"application/json"
+        }
+    };
+
+    fetch(url, params)
+        .then(response=>{
+            if (response.status !==200){
+                return null
+            } 
+            return response.json()
+        })
+        .then( result=>{
+            if (!result){
+                logOut();
+            } else{
+            const {accessToken, refreshToken} = result;
+            localStorage.setItem(ACCESS_TOKEN, accessToken);
+            localStorage.setItem(REFRESH_TOKEN, refreshToken);
+            }
+        })
+
+
+}
+
+
+export function logOut(){
+    localStorage.removeItem(ACCESS_TOKEN);
+    localStorage.removeItem(REFRESH_TOKEN);
 }
 
 function willExpireToken(token){
