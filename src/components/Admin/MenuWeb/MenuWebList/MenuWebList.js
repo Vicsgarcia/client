11cba09a9197f8ff81006161a37bfaf0ca280f5f
Victor Garcia <v.garcia.mestre@gmail.com>
2021-02-05 22:59:37 +0100

    Obteniendo todos los menús en el client y listándolos

---
+++ src/components/Admin/MenuWeb/MenuWebList/MenuWebList.js
@@ -0,0 +1,16 @@
+import React, {useState, useEffect} from 'react';
+
+
+export default function MenuWebList(props){
+    const {menu, setReloadMenuWeb} = props;
+    console.log(menu);
+
+    return(
+        <div>
+            <h1>Menu Web Listt</h1>
+            {menu.map(item =>(
+                <p key={item._id}>{item.title}</p>
+            ))}
+        </div>
+    )
+};
