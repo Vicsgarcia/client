095d093fc2aede347976d32afa4d78c55fba7dc4
Victor Garcia <v.garcia.mestre@gmail.com>
2021-02-05 16:31:57 +0100

    Delete user api

--- src/api/user.js
+++ src/api/user.js
@@ -168,7 +168,7 @@ export function activateUserApi(token, userId, status){
         body:JSON.stringify({
             active: status
         })
-    }
+    };
     return fetch(url, params).then(response =>{
         return response.json();
     }).then (result =>{
@@ -178,3 +178,22 @@ export function activateUserApi(token, userId, status){
     });
 
 }
+
+export function deleteUserApi(token, userId){
+    const url= `${basePath}/${apiVersion}/delete-user/${userId}`;
+    const params={
+        method:"DELETE",
+        headers:{
+            "Content-Type":"application/json",
+            Authorization: token
+        }
+    };
+    return fetch (url, params)
+    .then(response =>{
+        return response.json();
+    }). then (result =>{
+        return result.message
+    }). catch (err =>{
+        return err.message
+    });
+};
