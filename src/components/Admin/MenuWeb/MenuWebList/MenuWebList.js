393608e62bf07d69cd6e55b5a6822b4cb0597925
Victor Garcia <v.garcia.mestre@gmail.com>
2021-02-06 13:37:47 +0100

    updating menu order in front by draganddrop

--- src/components/Admin/MenuWeb/MenuWebList/MenuWebList.js
+++ src/components/Admin/MenuWeb/MenuWebList/MenuWebList.js
@@ -3,6 +3,9 @@ import {Switch, List, Button, Modal as ModalAntd, notification} from 'antd';
 import {EditOutlined, DeleteOutlined} from '@ant-design/icons';
 import Modal from '../../../Modal';
 import DragSortableList from 'react-drag-sortable';
+import {updateMenuApi} from '../../../../api/menu';
+import {getAccessTokenApi} from '../../../../api/auth';
+
 import './MenuWebList.scss';
 
 
@@ -33,7 +36,12 @@ export default function MenuWebList(props){
 
 
     const onSort = (sortedList, dropEvent)=>{
-        console.log(sortedList);
+        const accessToken= getAccessTokenApi()
+        sortedList.forEach(item =>{
+            const { _id } = item.content.props.item;
+            const order =item.rank;
+            updateMenuApi(accessToken, _id, {order});
+        })
     }
 
     return(
